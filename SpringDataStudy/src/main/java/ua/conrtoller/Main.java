0b47ac2195c86d4b3796df332ee24494e9fd01e4
Sergiy <sokol.serhij@gmail.com>
2016-07-28 21:23:06 +0300

    adf

--- SpringDataStudy/src/main/java/ua/conrtoller/Main.java
+++ SpringDataStudy/src/main/java/ua/conrtoller/Main.java
@@ -4,7 +4,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ua.entity.Country;
+import ua.entity.Recipe;
 import ua.repository.CountryRepository;
+import ua.repository.RecipeRepository;
 import ua.service.CountryService;
 
 public class Main {
@@ -12,11 +14,14 @@ public class Main {
 	static ConfigurableApplicationContext context = 
 			new ClassPathXmlApplicationContext
 			("/META-INF/applicationContext.xml");
-	static CountryService service = context.getBean(CountryService.class);
+	static RecipeRepository recipeRepository = context.getBean(RecipeRepository.class);
 	
 	
 	public static void main(String[] args) {
-		service.delete("Ukraine");
+		Recipe recipe = new Recipe();
+		recipe.setName("Borsch");
+		recipeRepository.save(recipe);
+		
 		context.close();
 	}
 
