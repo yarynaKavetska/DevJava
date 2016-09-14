feda49a6461413cd79f19fb9b2fa93dd5e2e8919
Sergiy <sokol.serhij@gmail.com>
2016-09-14 20:11:57 +0300

    sdf

--- MVC/src/main/java/ua/service/implementation/specification/IngredientAmountFilterAdapter.java
+++ MVC/src/main/java/ua/service/implementation/specification/IngredientAmountFilterAdapter.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -65,8 +66,8 @@ public class IngredientAmountFilterAdapter implements Specification<AmountAndIng
 	@Override
 	public Predicate toPredicate(Root<AmountAndIngredient> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
 		if (query.getResultType() != Long.class && query.getResultType() != long.class) {
-			root.fetch("ingredient");
-			root.fetch("measuringSystem");
+			root.fetch("ingredient", JoinType.LEFT);
+			root.fetch("measuringSystem", JoinType.LEFT);
 		}
 		findByAmount();
 		findByIngredient();
