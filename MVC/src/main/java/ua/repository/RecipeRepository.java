a2b0d16980634010f92fb884941dfea759de4eb5
Sergiy <sokol.serhij@gmail.com>
2016-09-22 21:52:31 +0300

    sdf

--- MVC/src/main/java/ua/repository/RecipeRepository.java
+++ MVC/src/main/java/ua/repository/RecipeRepository.java
@@ -1,6 +1,7 @@
 package ua.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -16,7 +17,7 @@ public interface RecipeRepository extends JpaRepository<Recipe, Integer>{
 //	@Query(value = "SELECT r FROM Recipe r LEFT JOIN FETCH r.country",
 //			countQuery="SELECT count(r.id) FROM Recipe r LEFT JOIN r.country")
 //	Page<Recipe> findAll(Pageable pageable);
-	
+	@Modifying
 	@Query("UPDATE Recipe r SET r.country = :country WHERE r.name = :name")
 	void changeCountry(@Param("name")String name, @Param("country")Country country);
 	
